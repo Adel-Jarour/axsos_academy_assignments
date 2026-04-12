5d085868c0c061d9bea6922be3cef2be76fdb091
adel <adel.jarour@gmail.com>
2026-04-12 19:15:23 +0300

    solve selection sort(optional)

---
+++ python_stack/python/python_fundamentals/selection_sort/selection_sort.py
@@ -0,0 +1,15 @@
+def selectionSort(lst):
+    n = len(lst)
+    
+    for i in range(n):
+        min_index = i
+        
+        for j in range(i + 1, n):
+            if lst[j] < lst[min_index]:
+                min_index = j
+        
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    
+    return lst
+
+print(selectionSort([8, 5, 2, 6, 4, 7, 0, 1, 3, 9]))
